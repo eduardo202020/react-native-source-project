98ce6f5910e0fdf865f02fafecdec1df96672700
Eduardo26 <jguevaral@uni.pe>
2023-06-14 19:14:26 -0500

    ADD react-native plugin to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    "react-native/react-native": true,
   },
   extends: [
     "eslint:recommended",
@@ -13,9 +14,17 @@ module.exports = {
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
-  plugins: ["@typescript-eslint", "react"],
+  plugins: ["@typescript-eslint", "react", "react-native"],
   rules: {
     "react/react-in-jsx-scope": "off",
+    "react-native/no-unused-styles": 2,
+    "react-native/split-platform-components": 2,
+    "react-native/no-inline-styles": 2,
+    "react-native/no-color-literals": 2,
+    "react-native/no-raw-text": 2,
   },
 }
